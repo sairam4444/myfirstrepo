9b396bfb9699805a07524b7f90107acbde185a87
sairam4444 <veerasairam1234@gmail.com>
2024-11-30 19:20:44 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/mydatabase')
+.then(() => console.log("connected"))
+.catch((error) => console.log("Connection error:", error));
+
+const bookSchema = new mongoose.Schema({
+  bookId: String,
+  title: String,
+  author: String,
+  publishedDate: String
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+app.post('/api/books', async (req, res) => {
+  try {
+    const book = new Book(req.body);
+    await book.save();
+    res.status(201).json(book);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get('/api/books', async (req, res) => {
+  try {
+    const book = await Book.find();
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/api/books/:bookId', async (req, res) => {
+  try {
+    const book = await Book.findOne({ bookId: req.params.bookId });
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put('/api/books/:bookId', async (req, res) => {
+  try {
+    const book = await Book.findOneAndUpdate(
+      { bookId: req.params.bookId },
+      req.body
+    );
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete('/api/books/:bookId', async (req, res) => {
+  try {
+    const book = await Book.findOneAndDelete({ bookId: req.params.bookId });
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.status(204).send();
+  } catch (error) {
+    console.error("Error deleting book:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
